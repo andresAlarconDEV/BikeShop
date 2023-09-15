83e765840f0cb5ea9cc6f5c3dbb7fc322a0c15e8
andresAlarconDEV <valarcon2712@gmail.com>
2023-09-14 22:05:32 -0500

    install de firebase

--- src/index.js
+++ src/index.js
@@ -4,6 +4,22 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB2EzCZuMe9lNYIZhIkFYVL9nOsH0ClNgY",
+  authDomain: "bikeshop-react.firebaseapp.com",
+  projectId: "bikeshop-react",
+  storageBucket: "bikeshop-react.appspot.com",
+  messagingSenderId: "1004943211373",
+  appId: "1:1004943211373:web:31b8c56b970da182f87864"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
